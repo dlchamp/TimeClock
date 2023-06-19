4113ae2e70b9e0f7d7fd5264b03c64d9a63ecdaa
DLCHAMP <36091350+dlchamp@users.noreply.github.com>
2023-06-18 22:27:21 -0500

    include env loading and version bump

--- timeclock/__init__.py
+++ timeclock/__init__.py
@@ -1,1 +1,20 @@
-__version__ = "0.1.0"
+from timeclock import log
+
+logger = log.get_logger(__name__)
+
+
+try:
+    import dotenv
+except ModuleNotFoundError:
+    pass
+
+else:
+    import dotenv
+
+    dotenv.load_dotenv(override=True)
+    logger.info(f"Environment variables loaded from .env file")
+
+
+__all__ = ()
+
+__version__ = "0.2.0"
