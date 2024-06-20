b387b5369635d303ac1965b066a38f0b840c9781
DLCHAMP <contact@dlchamp.com>
2024-06-20 07:29:18 -0500

    move dotenv loading to constants

--- timeclock/constants.py
+++ timeclock/constants.py
@@ -1,15 +1,28 @@
 import os
 
+import disnake
+
+from timeclock import log
+
+logger = log.get_logger(__name__)
+
+
+try:
+    import dotenv
+except ModuleNotFoundError:
+    pass
+
+else:
+    dotenv.load_dotenv(override=True)
+    logger.info(f"Environment variables loaded from .env file")
+
 
 class Client:
     token: str = os.getenv("TOKEN")
 
 
 class Database:
-    sql_bind = os.getenv("SQL_BIND")
-
-
-import disnake
+    sql_bind = "sqlite+aiosqlite:///timeclock/database/data.sqlite3"
 
 
 def default_embed():
